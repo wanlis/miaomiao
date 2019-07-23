6eb855a44da5ba0ce6feb590b4cbfce1f05a9f01
Daluobai <1002287979@qq.com>
2019-07-23 15:25:18 +0800

    add setData

--- src/components/Search/index.vue
+++ src/components/Search/index.vue
@@ -4,38 +4,24 @@
       <div class="search_input">
         <div class="search_input_wrapper">
           <i class="iconfont icon-sousuo"></i>
-          <input type="text" />
+          <input type="text" v-model="message" />
         </div>
       </div>
       <div class="search_result">
         <h3>电影/电视剧/综艺</h3>
         <ul>
-          <li>
+          <li v-for="item in moviesList" :key="item.id">
             <div class="img">
-              <img src="/images/movie_1.jpg" />
+              <img :src="item.img | setwh('128.180')" />
             </div>
             <div class="info">
               <p>
-                <span>无名之辈</span>
-                <span>8.5</span>
+                <span>{{item.nm}}</span>
+                <span>{{item.sc}}</span>
               </p>
-              <p>A Cool Fish</p>
-              <p>剧情,喜剧,犯罪</p>
-              <p>2018-11-16</p>
-            </div>
-          </li>
-          <li>
-            <div class="img">
-              <img src="/images/movie_1.jpg" />
-            </div>
-            <div class="info">
-              <p>
-                <span>无名之辈</span>
-                <span>8.5</span>
-              </p>
-              <p>A Cool Fish</p>
-              <p>剧情,喜剧,犯罪</p>
-              <p>2018-11-16</p>
+              <p>{{item.enm}}</p>
+              <p>{{item.cat}}</p>
+              <p>{{item.rt}}</p>
             </div>
           </li>
         </ul>
@@ -47,7 +33,25 @@
 <script>
 export default {
   name: "Search",
-  props: {}
+  props: {},
+  data() {
+    return {
+      moviesList: [],
+      message: ""
+    };
+  },
+  watch: {
+    message(newval) {
+      this.axios.get("/api/searchList?cityId=10&kw=" + newval).then(res => {
+        var msg = res.data.msg;
+        var data = res.data.data;
+        if (data && msg) {
+          this.moviesList = res.data.data.movies.list;
+        }
+      });
+    }
+  },
+  mounted() {}
 };
 </script>
 
